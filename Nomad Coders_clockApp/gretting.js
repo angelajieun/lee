fe50a2044344775d5635d4d2f9269182e3a7216a
angelajieun <ji918vgod@gmail.com>
2019-01-19 17:44:25 +0900

    vanillaJS 로 app 만들기

---
+++ Nomad Coders_clockApp/gretting.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".js-form"),
+      input = form.querySelector("input");
+
+const User_LS = "currentUset";
+function loadName(){
+  const currentUser = localStorage.getItem(User_LS);
+  if (User_LS === null){
+    
+  }else {
+
+  }
+}
+
+function init(
+  loadName();
+);
+init();
