28f210152a729dbec67f1d1119a2ae7bbecd8527
angelajieun <ji918vgod@gmail.com>
2019-01-20 16:10:59 +0900

    localStorage 에 이름 저장하기
    localStorage.setItem();
    localStorage.getItem();
    .classList.remove();
    .classList.add();
    .innerText();

--- Nomad Coders_clockApp/gretting.js
+++ Nomad Coders_clockApp/gretting.js
@@ -1,17 +1,39 @@
 const form = document.querySelector(".js-form"),
-      input = form.querySelector("input");
+      input = form.querySelector("input"),
+      greeting = document.querySelector('.js-greetings');
 
-const User_LS = "currentUset";
+const User_LS = "currentUser";
+const Showing_CN = "showing";
+
+function saveName(text){
+  localStorage.setItem(User_LS,text);
+}
+function handleSubmit(event){ // form ㅇㅔ서 submit 하면 그 위의 
+  event.preventDefault();
+  //input.placeholder="" / input.value
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+function askForName(){
+  form.classList.add(Showing_CN);
+  form.addEventListener('submit', handleSubmit);
+}
+function paintGreeting(text){
+  form.classList.remove(Showing_CN)
+  greeting.classList.add(Showing_CN);
+  greeting.innerText = `Hello ${text}`;
+}
 function loadName(){
   const currentUser = localStorage.getItem(User_LS);
-  if (User_LS === null){
-    
+  if (currentUser === null){
+    askForName();
   }else {
-
+    paintGreeting(currentUser);
   }
 }
 
-function init(
+function init(){
   loadName();
-);
+}
 init();
